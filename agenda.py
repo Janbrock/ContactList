0dc13eddaffb4c84942fdd9ceca6e8fa475f1a19
Janbrock <carbonoeditor@gmail.com>
2026-03-24 13:49:06 -0300

    Usando while para manter o menu rodando

--- agenda.py
+++ agenda.py
@@ -15,13 +15,14 @@ AGENDA['Maria'] = {
 def mostrar_contatos():
     for contato in AGENDA:
         buscar_contato(contato)
-        print('------------------')
+       
 
 def buscar_contato(contato):
         print('Nome:', contato)
         print('Telefone:', AGENDA[contato]['telefone'])
         print('Email:', AGENDA[contato]['email'])
         print('Endereço:', AGENDA[contato]['endereco'])
+        print('--------------------------------------')
 
 def incluir_editar_contato(contato, telefone, email, endereco):
     AGENDA[contato] = {
@@ -29,44 +30,53 @@ def incluir_editar_contato(contato, telefone, email, endereco):
     'email': email,
     'endereco': endereco
     }
+    print()
     print('>>>> Contato {} adicionado/editado com sucesso'.format(contato))
+    print()
      
 def excluir_contato(contato):
     AGENDA.pop(contato)
+    print()
     print('>>>> Contato {} excluído com sucesso'.format(contato))
+    print()
 
 def imprimir_menu():
     print('''
+          
         1 - Mostrar todos os contatos da agenda
         2 - Buscar contato
         3 - Adicionar contato
         4 - Editar contato
         5 - Excluir contato
         0 - Fechar agenda 
+          
         '''
     )
 
-imprimir_menu()
+while True:
+    imprimir_menu()
 
-# sempre que sai do INPUT sai como STRING
-opcao = input('Escolha uma opção:')
+    opcao = input('Escolha uma opção:')
 
-if opcao == '1':
-     mostrar_contatos()
-elif opcao == '2':
-     contato = input('Digite o nome do contato:')
-     buscar_contato(contato)
-elif opcao == '3' or opcao == '4':
-     nome = input('Digite o nome:')
-     telefone = input('Digite o telefone:')
-     email = input('Digite o email:')
-     endereco = input('Digite o endereço:')
-     incluir_editar_contato(nome, telefone, email, endereco)
-elif opcao == '5':
-     contato = input('Digite o nome do contato:')
-     excluir_contato(contato)
-elif opcao == '0':
-     exit
-else:
-     print('Opção inválida.')
+    if opcao == '1':
+        mostrar_contatos()
+    elif opcao == '2':
+        contato = input('Digite o nome do contato:')
+        buscar_contato(contato)
+    elif opcao == '3' or opcao == '4':
+        nome = input('Digite o nome:')
+        telefone = input('Digite o telefone:')
+        email = input('Digite o email:')
+        endereco = input('Digite o endereço:')
+        incluir_editar_contato(nome, telefone, email, endereco)
+    elif opcao == '5':
+        contato = input('Digite o nome do contato:')
+        excluir_contato(contato)
+    elif opcao == '0':
+        print('>>>> Fechando o programa...')
+        break
+    else:
+        print()
+        print('>>>> Opção inválida.')
+        print()
 
