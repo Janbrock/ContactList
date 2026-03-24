081dc5aaae430ae31176fddee048a8acd936cc43
Janbrock <carbonoeditor@gmail.com>
2026-03-24 12:15:59 -0300

    Criando método para incluir contato

--- agenda.py
+++ agenda.py
@@ -1,22 +1,16 @@
 AGENDA = {}
 
-# CRIANDO UM AAGENDA NA MÃO
-
-AGENDA['guilherme'] = {
+AGENDA['Guilherme'] = {
     'telefone': '99999-9999',
     'email': 'guilherme@python.com.br',
     'endereco': 'Av. 1'
 }
 
-AGENDA['maria'] = {
+AGENDA['Maria'] = {
     'telefone': '99999-9998',
     'email': 'maria@python.com.br',
     'endereco': 'Av. 2'
 }
-
-# DEF = DEFINIR
-# PASS = PASSA
-# CRIANDO MÉTODO PARA MOSTRAR CONTATOS
 
 def mostrar_contatos():
     for contato in AGENDA:
@@ -29,5 +23,14 @@ def buscar_contato(contato):
         print('Email:', AGENDA[contato]['email'])
         print('Endereço:', AGENDA[contato]['endereco'])
 
+def incluir_contato(contato, telefone, email, endereco):
+    AGENDA[contato] = {
+    'telefone': telefone,
+    'email': email,
+    'endereco': endereco
+    }
+    print('>>>> Contato {} adicionado com sucesso'.format(contato))
+
 mostrar_contatos()
-# buscar_contato('maria')
+incluir_contato('Joana', '988887777', 'joana@python.com.br', 'Av. 3')
+mostrar_contatos()
