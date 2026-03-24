e8ccef66c93ccbc3aff8b0a1a06710d49eee9c97
Janbrock <carbonoeditor@gmail.com>
2026-03-24 11:15:16 -0300

    Criação e manipulação de dicionário

---
+++ dicionario.py
@@ -0,0 +1,17 @@
+# chave : valor
+pessoas = {
+    "guilherme": 19,
+    "maria": 17,
+    "joão": 20,
+    }
+
+# imprimindo valor através de chave
+# print(dicionario['maria'])
+
+# iteração com for
+# for pessoa in pessoas:
+#     print(pessoa)
+
+# iteração em valores
+for idade in pessoas.values():
+    print(idade)
